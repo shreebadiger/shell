c53f084b6b6733cb9967fd940bebc11d478fe415
shree <shreesbadiger@gmail.com>
2023-11-09 22:30:36 +0530

    updated

--- backend.sh
+++ backend.sh
@@ -1,40 +1,56 @@
-echo -e "\e[36m Disabling default nodejs \e[0m"
-dnf module disable nodejs -y &>>/tmp/expense.log
+log_file=/tmp/expense.log
+color="\e[35m"
 
-echo -e "\e[36m Copying backend service file \e[0m"
-cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 
-echo -e "\e[36m Enabling nodejs required version \e[0m"
-dnf module enable nodejs:18 -y &>>/tmp/expense.log
 
-echo -e "\e[36m Installing nodejs \e[0m"
-dnf install nodejs -y &>>/tmp/expense.log
+echo -e "${color} Disabling default nodejs \e[0m"
+dnf module disable nodejs -y &>>log_file
+echo $?
 
-echo -e "\e[36m Creating user to run application \e[0m"
-useradd expense &>>/tmp/expense.log
+echo -e "${color} Copying backend service file \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>log_file
+echo $?
 
-echo -e "\e[36m Creating directory for the application \e[0m"
-mkdir /app &>>/tmp/expense.log
+echo -e "${color} Enabling nodejs required version \e[0m"
+dnf module enable nodejs:18 -y &>>log_file
+echo $?
 
-echo -e "\e[36m Downloading file \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip  &>>/tmp/expense.log
+echo -e "${color} Installing nodejs \e[0m"
+dnf install nodejs -y &>>log_file
+echo $?
 
-echo -e "\e[36m Extracting the file \e[0m"
-cd /app  &>>/tmp/expense.log
-unzip /tmp/backend.zip &>>/tmp/expense.log
+echo -e "${color} Creating user to run application \e[0m"
+useradd expense &>>log_file
+echo $?
 
-echo -e "\e[36m Installing dependencies \e[0m"
-cd /app &>>/tmp/expense.log
-npm install  &>>/tmp/expense.log
+echo -e "${color} Creating directory for the application \e[0m"
+mkdir /app &>>log_file
+echo $?
 
-echo -e "\e[36m Starting backend service \e[0m"
-systemctl daemon-reload &>>/tmp/expense.log
-systemctl enable backend &>>/tmp/expense.log
-systemctl restart backend &>>/tmp/expense.log
+echo -e "${color}Downloading file \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip  &>>log_file
+echo $?
 
-dnf install mysql -y &>>/tmp/expense.log
+echo -e "${color} Extracting the file \e[0m"
+cd /app  &>>/log_file
+unzip /tmp/backend.zip &>>log_file
+echo $?
 
-echo -e "\e[36m Loading the schema \e[0m"
-mysql -h mysql-dev.sbadiger93.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
+echo -e "${color} Installing dependencies \e[0m"
+cd /app &>>log_file
+npm install  &>>log_file
+echo $?
 
+echo -e "${color} Starting backend service \e[0m"
+systemctl daemon-reload &>>log_file
+systemctl enable backend &>>log_file
+systemctl restart backend &>>log_file
+echo $?
+
+dnf install mysql -y &>>log_file
+echo $?
+
+echo -e "${color} Loading the schema \e[0m"
+mysql -h mysql-dev.sbadiger93.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
+echo $?
 
