e76f6bfd7c37aa8185a85ea9f204f0f231e8e5cf
shree <shreesbadiger@gmail.com>
2023-11-09 22:25:46 +0530

    updated

--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,26 @@
-echo -e "\e[36m Disabling default mysql version \e[0m"
-dnf module disable mysql -y &>>/tmp/expense.log
+log_file=/tmp/expense.log
+color="\e[35m"
 
-echo -e "\e[36m Creating mysql repo \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/expense.log
+echo -e "${color}Disabling default mysql version \e[0m"
+dnf module disable mysql -y &>>log_file
+echo $?
 
-echo -e "\e[36m Installing mysql community server \e[0m"
-dnf install mysql-community-server -y &>>/tmp/expense.log
+echo -e "${color} Creating mysql repo \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
+echo $?
 
-echo -e "\e[36m ENABLING MYSQL server \e[0m"
-systemctl enable mysqld &>>/tmp/expense.log
+echo -e "${color} Installing mysql community server \e[0m"
+dnf install mysql-community-server -y &>>log_file
+echo $?
 
-echo -e "\e[36m Starting mysql server \e[0m"
-systemctl start mysqld &>>/tmp/expense.log
+echo -e "${color} ENABLING MYSQL server \e[0m"
+systemctl enable mysqld &>>log_file
+echo $?
 
-echo -e "\e[36m setting password to mysql \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
+echo -e "${color} Starting mysql server \e[0m"
+systemctl start mysqld &>>log_file
+echo $?
+
+echo -e "${color} setting password to mysql \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
+echo $?
