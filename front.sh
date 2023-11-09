1f6b9b631bb2ce97c68d8f4e9c013248afccaf44
shree <shreesbadiger@gmail.com>
2023-11-09 22:47:55 +0530

    updated

--- front.sh
+++ front.sh
@@ -2,27 +2,26 @@ log_file=/tmp/expense.log
 color="\e[36m"
 
 echo -e "${color}Installing nginx \e[0m"
-dnf install nginx -y &>>log_file
+dnf install nginx -y &>>$log_file
 echo $?
 
 echo -e "${color} copying expense configuration file nginx \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 echo $?
 
 echo -e "\e[36m Removing default nginx webpage  \e[0m"
-rm -rf /usr/share/nginx/html/* &>>log_file
-echo $?
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 echo -e "${color} Downloading application content \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip  &>>log_file
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip  &>>$log_file
 echo $?
 
 echo -e "${color} Extracting content \e[0m" 
-cd /usr/share/nginx/html &>>log_file
-unzip /tmp/frontend.zip &>>log_file
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
 echo $?
 
 echo -e "${color} Starting nginx  \e[0m" 
-systemctl enable nginx &>>log_file
-systemctl restart nginx &>>log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 echo $?
