bd4d976fb818ea86b1e0ef6f0e161eb4a5d5ac14
shree <shreesbadiger@gmail.com>
2023-11-10 15:08:12 +0530

    updated

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,4 @@
-log_file=/tmp/expense.log
-color="\e[35m"
-status_check() {
- if [ $? -eq 0 ]; then
-   echo -e "\e[31m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
-}
+source common.sh
 
 echo -e "${color}Disabling default mysql version \e[0m"
 dnf module disable mysql -y &>>$log_file
