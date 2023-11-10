78ba2818ac18331d09998001b68fb8eb82c74aff
shree <shreesbadiger@gmail.com>
2023-11-10 15:07:41 +0530

    updated

--- front.sh
+++ front.sh
@@ -1,13 +1,4 @@
-log_file=/tmp/expense.log
-color="\e[36m"
-
-status_check() {
- if [ $? -eq 0 ]; then
-   echo -e "\e[31m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
- fi
-}
+source common.sh
 
 echo -e "${color}Installing nginx \e[0m"
 dnf install nginx -y &>>$log_file
